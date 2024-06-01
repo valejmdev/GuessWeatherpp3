81241d7bde4692ca39c9e415c41c00619e7ba69a
valejmdev Müller <valentinjmueller055@gmail.com>
2024-06-01 12:54:39 +0000

    create welcome message

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""This weather guessing game with random locations"""
+print("Welcome to the WeaterGuesser!")
+
+name = input("Enter your name: ")
+
+print("Hello, " + name + "! "
+      "Welcome again to the WeatherGuesser. "
+      "Following you will guess the weather in a "
+      "random location from all over the world!")
