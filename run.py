4eba35dba125ad975ebea343ff3557d7e1fcfe0f
valejmdev Müller <valentinjmueller055@gmail.com>
2024-06-02 13:39:22 +0000

    add comments for api integration testing

--- run.py
+++ run.py
@@ -2,6 +2,9 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 """This weather guessing game with random locations"""
+
+
+# Welcome Message
 print("Welcome to the WeaterGuesser!")
 
 
@@ -30,12 +33,14 @@ while True:
 current_city = "Zwickau"
 
 
+# Class for questions
 class Question:
     def __init__(self, question, answer):
         self.question = question
         self.answer = answer
 
 
+# Array for questions
 weather_prompt = [
     "How is the weather for the day in:" + current_city +
     "\n(1) Sunny\n(2) Mostly Sunny\n(3) Mostly Cloudy\n(4) Overcast\n",
@@ -43,13 +48,14 @@ weather_prompt = [
     "\n(1) less than 10°C\n(2) 10°C-20°C\n(3) 20°C-30°C\n(4) more than 30°C\n"
     ]
 
-
+# Correct answer validation
 questions_validation = [
     Question(weather_prompt[0], "2"),
     Question(weather_prompt[1], "3")
 ]
 
 
+# Function of the guesser game
 def run_guesser(questions_validation):
     score = 0
     for question in questions_validation:
@@ -60,4 +66,5 @@ def run_guesser(questions_validation):
           " correct")
 
 
+# Calling guesser game function
 run_guesser(questions_validation)
