ab51e4d64025a43350ec923e1c59449433e56549
valejmdev Müller <valentinjmueller055@gmail.com>
2024-06-02 14:29:16 +0000

    test to get data from api and display it

--- run.py
+++ run.py
@@ -37,7 +37,34 @@ while True:
     else:
         print("Username should contain 3 to 16 alphabetical characters only.")
 
-current_city = "Zwickau"
+
+# City name input for testing
+city_name = input("What city do you want to guess for? ")
+
+# Building url for testing api call
+url = f"{root_url}appid={api_key}&q={city_name}"
+
+# Sending a get request at the url
+r = requests.get(url)
+
+# Displaying the json weather data returned by the api
+print(r.json())
+
+data = r.json()
+
+# Checking if there is no error and the status code is 200
+if data['cod'] == 200:
+    # Getting the temperature form the json data
+    temp = data['main']['temp']
+    # Getting the description of the weather from the json data
+    descr = data['weather'][0]['description']
+
+    print(f"City Name: {city_name}")
+    print(f"Weather Condition is {descr}")
+    print(f"The temperatur is: {temp}")
+
+else:
+    print("Something went wrong... Please try again...")
 
 
 # Class for questions
@@ -49,9 +76,9 @@ class Question:
 
 # Array for questions
 weather_prompt = [
-    "How is the weather for the day in:" + current_city +
+    "How is the weather for the day in:" + city_name +
     "\n(1) Sunny\n(2) Mostly Sunny\n(3) Mostly Cloudy\n(4) Overcast\n",
-    "How warm is it for the day in:" + current_city +
+    "How warm is it for the day in:" + city_name +
     "\n(1) less than 10°C\n(2) 10°C-20°C\n(3) 20°C-30°C\n(4) more than 30°C\n"
     ]
 
